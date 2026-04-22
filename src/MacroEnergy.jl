9186980a272543bcf75a2f1d9256638b3eef7358
Jun Wen Law <junlaw@mit.edu>
2026-04-22 16:19:38 -0400

    New fossil refinery asset and updates to H2 assets
    Fossil refinery asset to fix user defined ratios of fossil gasoline, jet fuel and diesel. Added fuel edge for NG input in BECCS H2 asset, updated electrolyzer asset to include UC option.

--- src/MacroEnergy.jl
+++ src/MacroEnergy.jl
@@ -214,6 +214,7 @@ include("model/assets/hydrores.jl")
 include("model/assets/mustrun.jl")
 include("model/assets/upstreamemissions.jl")
 include("model/assets/downstreamemissions.jl")
+include("model/assets/fossilliquidfuelsrefinery.jl")
 include("model/assets/syntheticnaturalgas.jl")
 include("model/assets/syntheticliquidfuels.jl")
 include("model/assets/syntheticammonia.jl")
@@ -286,6 +287,7 @@ export AbstractAsset,
     ElectricHeating,
     ElectricSteam,
     UpstreamEmissions,
+    FossilLiquidFuelsRefinery,
     FuelCell,
     DownstreamEmissions,
     ThermalHeating,
